8dd79b50ba7e3144c446145e118f27d6aefb8380
Syed47 <syedbaryalay47@gmail.com>
2024-06-25 16:26:44 +0100

    issue fixed in Stage class

--- src/Stage.py
+++ src/Stage.py
@@ -1,18 +1,21 @@
 from pycromanager import Core
 
 class Stage:
-    def __init__(self, core:Core):
+    def __init__(self, core: Core):
         self.core = core
-        self.vertical = self.core.get_focus_device()
-        self.horizontal = self.core.get_xy_stage_device()
-        self.x = self.core.get_x_position(self.horizontal)
-        self.x = self.core.get_y_position(self.horizontal)
-        self.z = self.core.get_position(self.vertical)
+        self.focus_device = self.core.get_focus_device()
+        self.xy_stage_device = self.core.get_xy_stage_device()
+        self.x = self.core.get_x_position(self.xy_stage_device)
+        self.y = self.core.get_y_position(self.xy_stage_device)
+        self.z = self.core.get_position(self.focus_device)
     
     def move(self, x=None, y=None, z=None):
-        self.x = self.core.get_x_position(self.horizontal) if x == None else x
-        self.y = self.core.get_y_position(self.horizontal) if y == None else y
-        self.z = self.core.get_position(self.vertical) if z == None else z
-        self.core.set_xy_position(self.horizontal, self.x, self.y)
-        self.core.set_position(self.vertical, self.z)
-
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+        if z is not None:
+            self.z = z
+        
+        self.core.set_xy_position(self.xy_stage_device, self.x, self.y)
+        self.core.set_position(self.focus_device, self.z)
